2828f23f231067be250798cdb6afbfb747e63923
Ilia Ilin <ilia@ilin.dn.ua>
2020-08-12 17:32:53 +0300

    commit

---
+++ rabbitmq.go
@@ -0,0 +1,213 @@
+package rabbitmq
+
+import (
+	"bytes"
+	"time"
+	"log"
+
+	"github.com/streadway/amqp"
+	"sync/atomic"
+)
+
+const delay = 3 // reconnect after delay seconds
+
+// LogFunc is a function that logs the provided message with optional
+// fmt.Sprintf-style arguments. By default, logs to the default log.Logger.
+var LogFuncInfo func(string, ...interface{}) = log.Printf
+var LogFuncError func(string, ...interface{}) = log.Printf
+
+type Config struct {
+	SSL    bool
+	Addr   string
+	User   string
+	Passwd string
+}
+
+func (cfg *Config) FormatURL() string {
+	var buf bytes.Buffer
+
+	// [ampq[s]://]
+	buf.WriteString("amqp")
+
+	if cfg.SSL {
+		buf.WriteByte('s')
+	}
+	buf.WriteString("://")
+
+	// user:password@
+	if len(cfg.User) > 0 {
+		buf.WriteString(cfg.User)
+		if len(cfg.Passwd) > 0 {
+			buf.WriteByte(':')
+			buf.WriteString(cfg.Passwd)
+		}
+		buf.WriteByte('@')
+	}
+
+	// [address:port/]
+	buf.WriteString(cfg.Addr)
+	buf.WriteByte('/')
+
+	return buf.String()
+}
+
+// Connection amqp.Connection wrapper
+type Connection struct {
+	*amqp.Connection
+}
+
+// Dial wrap amqp.Dial, dial and get a reconnect connection
+func Dial(url string) (*Connection, error) {
+
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+	LogFuncInfo("rabbitmq connection success")
+	connection := &Connection{
+		Connection: conn,
+	}
+
+	go func() {
+		for {
+			reason, ok := <-connection.Connection.NotifyClose(make(chan *amqp.Error))
+			// exit this goroutine if closed by developer
+			if !ok {
+				LogFuncInfo("rabbitmq connection closed")
+				break
+			}
+			LogFuncError("rabbitmq connection closed, reason: %v", reason)
+
+			// reconnect if not closed by developer
+			for {
+				// wait 1s for reconnect
+				time.Sleep(delay * time.Second)
+
+				conn, err := amqp.Dial(url)
+				if err == nil {
+					connection.Connection = conn
+					LogFuncInfo("rabbitmq connection success")
+					break
+				}
+				LogFuncError("rabbitmq reconnect failed, err: %v", err)
+			}
+		}
+	}()
+
+	return connection, nil
+}
+
+// Channel wrap amqp.Connection.Channel, get a auto reconnect channel
+func (c *Connection) Channel() (*Channel, error) {
+	ch, err := c.Connection.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	LogFuncInfo("rabbitmq channel opened")
+	channel := &Channel{
+		Channel: ch,
+	}
+
+	go func() {
+		for {
+			reason, ok := <-channel.Channel.NotifyClose(make(chan *amqp.Error))
+			// exit this goroutine if closed by developer
+			if !ok || channel.IsClosed() {
+				LogFuncInfo("channel closed")
+				channel.Close() // close again, ensure closed flag set when connection closed
+				break
+			}
+			LogFuncError("channel closed, reason: %v", reason)
+
+			// reconnect if not closed by developer
+			for {
+				// wait 1s for connection reconnect
+				time.Sleep(delay * time.Second)
+
+				ch, err := c.Connection.Channel()
+				if err == nil {
+					LogFuncInfo("channel recreate success")
+					channel.Channel = ch
+					break
+				}
+
+				LogFuncError("channel recreate failed, err: %v", err)
+			}
+		}
+
+	}()
+
+	return channel, nil
+}
+
+// Channel amqp.Channel wapper
+type Channel struct {
+	*amqp.Channel
+	closed int32
+}
+
+// IsClosed indicate closed by developer
+func (ch *Channel) IsClosed() bool {
+	return (atomic.LoadInt32(&ch.closed) == 1)
+}
+
+// Close ensure closed flag set
+func (ch *Channel) Close() error {
+	if ch.IsClosed() {
+		return amqp.ErrClosed
+	}
+
+	atomic.StoreInt32(&ch.closed, 1)
+
+	return ch.Channel.Close()
+}
+
+// Consume warp amqp.Channel.Consume, the returned delivery will end only when channel closed by developer
+func (ch *Channel) Consume(queue, consumer string, autoAck, exclusive, noLocal, noWait bool, args amqp.Table) (<-chan amqp.Delivery, error) {
+	deliveries := make(chan amqp.Delivery)
+
+	go func() {
+		for {
+			d, err := ch.Channel.Consume(queue, consumer, autoAck, exclusive, noLocal, noWait, args)
+			if err != nil {
+				LogFuncError("consume failed, err: %v", err)
+				time.Sleep(delay * time.Second)
+				continue
+			}
+
+			for msg := range d {
+				deliveries <- msg
+			}
+
+			// sleep before IsClose call. closed flag may not set before sleep.
+			time.Sleep(delay * time.Second)
+
+			if ch.IsClosed() {
+				break
+			}
+		}
+	}()
+
+	return deliveries, nil
+}
+
+// func (ch *Channel) Publish(exchange, key string, mandatory, immediate bool, msg) error {
+
+// 	go func() {
+// 		for {
+// 			err := ch.Channel.Publish(exchange, key, mandatory, immediate, msg)
+// 			if err != nil {
+// 				// log.Error().Msgf("publish filed, err: %v", err)
+// 				time.Sleep(delay * time.Second)
+// 				continue
+// 			}
+
+// 			if ch.IsClosed() {
+// 				break
+// 			}
+// 		}
+// 	}()
+
+// 	return nil
+// }
---
+++ utils.go
@@ -0,0 +1,133 @@
+package rabbitmq
+
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
+func OpenConnect(uri string) ( *Channel, error ) {
+
+	conn, err := Dial(uri)
+	if err != nil {
+		return nil, err
+	} 
+
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+	}
+
+	return ch, nil
+}
+
+func (ch *Channel) InitStruct(prefix string) error {
+	var err error
+
+	LogFuncInfo("rabbitmq struct initing start")
+
+	err = ch.ExchangeDeclare(
+		fmt.Sprintf("%sExchange", prefix), // name
+		"fanout",                          // type
+		true,                              // durable
+		false,                             // auto delete
+		false,                             // internal
+		false,                             // no wait
+		nil,                               // arguments
+	)
+	if err != nil {
+		return err
+	}
+
+	err = ch.ExchangeDeclare(
+		fmt.Sprintf("%sRetryExchange", prefix), // name
+		"fanout",                               // type
+		true,                                   // durable
+		false,                                  // auto delete
+		false,                                  // internal
+		false,                                  // no wait
+		nil,                                    // arguments
+	)
+	if err != nil {
+		return err
+	}
+
+	args := amqp.Table{
+		"x-dead-letter-exchange": fmt.Sprintf("%sRetryExchange", prefix),
+	}
+	_, err = ch.QueueDeclare(
+		fmt.Sprintf("%sQueue", prefix), // name
+		true,                           // durable - flush to disk
+		false,                          // delete when unused
+		false,                          // exclusive - only accessible by the connection that declares
+		false,                          // no-wait - the queue will assume to be declared on the server
+		args,                           // arguments -
+	)
+	if err != nil {
+		return err
+	}
+
+	args = amqp.Table{
+		"x-dead-letter-exchange": fmt.Sprintf("%sExchange", prefix),
+		"x-message-ttl":          60000,
+	}
+	_, err = ch.QueueDeclare(
+		fmt.Sprintf("%sRetryQueue", prefix), // name
+		true,                                // durable - flush to disk
+		false,                               // delete when unused
+		false,                               // exclusive - only accessible by the connection that declares
+		false,                               // no-wait - the queue will assume to be declared on the server
+		args,                                // arguments -
+	)
+	if err != nil {
+		return err
+	}
+
+	_, err = ch.QueueDeclare(
+		fmt.Sprintf("%sArchiveQueue", prefix), // name
+		true,                                  // durable - flush to disk
+		false,                                 // delete when unused
+		false,                                 // exclusive - only accessible by the connection that declares
+		false,                                 // no-wait - the queue will assume to be declared on the server
+		nil,                                   // arguments -
+	)
+	if err != nil {
+		return err
+	}
+
+	err = ch.QueueBind(
+		fmt.Sprintf("%sQueue", prefix),
+		"*",
+		fmt.Sprintf("%sExchange", prefix),
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	err = ch.QueueBind(
+		fmt.Sprintf("%sArchiveQueue", prefix),
+		"*",
+		fmt.Sprintf("%sExchange", prefix),
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	err = ch.QueueBind(
+		fmt.Sprintf("%sRetryQueue", prefix),
+		"*",
+		fmt.Sprintf("%sRetryExchange", prefix),
+		false,
+		nil,
+	)
+	if err != nil {
+		return err
+	}
+
+	LogFuncInfo("rabbitmq struct initing success")
+	return nil
+}
